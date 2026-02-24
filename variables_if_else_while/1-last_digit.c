5ba10a83a61d9759c6c9dd914af8fea09f6f8252
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-02-24 15:05:03 +0100

    fix: rewrite to correspond the exercise

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,6 +21,6 @@ int main(void)
 	if (lastdigit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n",n, lastdigit);
 	if (lastdigit < 6)
-		printf("Last digit of %d is %d and less than 6 and not 0\n",n, lastdigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",n, lastdigit);
 	return (0);
 }
