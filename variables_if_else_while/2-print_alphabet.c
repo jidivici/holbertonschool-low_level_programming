12498a6d202790a609d9712b8afb9222a1f4678d
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-02-24 15:15:34 +0100

    add: rewrite alphabet with putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: 0 (sucess)
+*/
+int main(void)
+{
+	putchar('a');
+	putchar('b');
+	putchar('c');
+	putchar('d');
+	putchar('e');
+	putchar('f');
+	putchar('g');
+	putchar('h');
+	putchar('i');
+	putchar('j');
+	putchar('k');
+	putchar('l');
+	putchar('m');
+	putchar('n');
+	putchar('o');
+	putchar('p');
+	putchar('q');
+	putchar('r');
+	putchar('s');
+	putchar('t');
+	putchar('u');
+	putchar('v');
+	putchar('w');
+	putchar('x');
+	putchar('y');
+	putchar('z');
+	putchar('\n');
+	return (0);
+}
