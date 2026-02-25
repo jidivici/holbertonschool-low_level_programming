6ea0a93a3c8d960c4fe9443020a94f81b4469982
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-02-25 15:28:34 +0100

    add: Write a function that checks for lowercase character.

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* _islower -  S/output
+* i: num of rep
+* c: select letter
+*
+* Return: None
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
