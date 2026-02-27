f385be67835fdc1b746c61aedd5fe339ac87aa7d
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-02-27 09:57:14 +0100

    add:a function that adds two integers and returns the result.

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* add - print table of 9 with putchar
+* @num1: first nomber
+* @num2: second number
+*
+* Return: int positive or neg
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
