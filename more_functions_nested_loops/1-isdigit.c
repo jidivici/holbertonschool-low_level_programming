7f0950b618d9cbcf0c4f8a5160f752427fe0ac98
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-02-27 17:43:44 +0100

    add: main.h README and function return 1 if c is a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit - check caracter if is digit or not
+* @c: the caracter tested
+*
+* Return: 1(sucess) or 0 (fail)
+*/
+int _isdigit(int c)
+{
+	if ((c >= '0' && c <= '9'))
+		return (1);
+	else
+		return (0);
+}
