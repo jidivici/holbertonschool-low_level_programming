9c9d573225ef0ceea4eae97ee6bc81e560417ae0
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-02-28 14:25:33 +0100

    fix: range of loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i = 0;
 
-	for (; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 		{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -20,5 +20,6 @@ int main(void)
 		else
 			printf("%d ", i);
 		}
+		printf("\n");
 	return (0);
 }
