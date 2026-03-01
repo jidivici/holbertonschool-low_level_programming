d25ee3e5b676b62bbe348e356dece2a89ed2246a
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-01 10:39:03 +0100

    add: program calculator can add substract multiplicate and divid 2 int and I use scanf and the arithmetic combinaison switch

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,31 +1,83 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* total_sum - sum * row
-* @rows: ligne
-* @cols: colonne
-*
-* Return: total
-*/
-int operation(int code)
+ * switch_ope - executes operation based on user choice
+ * @code: operation number selected by the user
+ *
+ * Return: 1 after executing operation
+ */
+int switch_ope(int code)
+{
+	switch (code)
+	{
+		case 1:
+			addition();
+			return (1);
+		case 2:
+			substract();
+			return (1);
+		case 3:
+			multiplication();
+			return (1);
+		case 4:
+			division();
+			return (1);
+	}
+	return (1);
+}
+/**
+ * print_menu - displays calculator menu
+ *
+ * Return: nothing
+ */
+void print_menu(void)
 {
 	printf("1) Add\n");
 	printf("2) Subtract\n");
 	printf("3) Multiply\n");
 	printf("4) Divide\n");
 	printf("0) Quit\n");
-	scanf("%d", &code);
-	printf("%d\n", code);
-	return code;
+	printf("Choice: ");
 }
-
-int main(void)
+/**
+ * checker - validates user choice
+ * @code: operation number entered by the user
+ *
+ * Return: 0 if user quits, 1 otherwise
+ */
+int checker(int code)
 {
-	int code = 0;
-
-	while (1)
+	if (code == 0)
 	{
-		operation(code);
+		printf("Bye!\n");
 		return (0);
 	}
+	else if (code > 4 || code < 0)
+		{
+		printf("Invalid choice\n");
+		printf("Choice: ");
+		return (0);
+		}
+	return (1);
+}
+/**
+ * main - program entry point
+ *
+ * Return: 0 when program exits
+ */
+int main(void)
+{
+	int code = 1;
+
+	print_menu();
+	while (code != 0)
+	{
+		scanf("%d", &code);
+		if (checker(code))
+		{
+			if (switch_ope(code))
+				print_menu();
+		}
+	}
+	return (0);
 }
--- simple-calculator/main.h
+++ simple-calculator/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void division(void);
+void multiplication(void);
+void substract(void);
+void addition(void);
+int switch_ope(int code);
+void print_menu(void);
+int checker(int code);
 
 #endif
