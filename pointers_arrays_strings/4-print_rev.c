37cc6be0d886bae5fce61e54dfb033b08cca3aea
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-02 15:49:01 +0100

    add: function _puts took *s and print it

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* _puts - print string with \n
+* @s: pointer str print
+*
+* Return: result of a * b
+*/
+int _puts(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
