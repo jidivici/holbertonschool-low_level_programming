07a86886aed514294ba82c3a84008dcea93970dc
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-02 21:04:37 +0100

    add: function print array with printf

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_array - print int array
+* @a: array you want print
+* @n: taille de l array
+*
+* Return: nothing
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+		printf("%d, /n", a[i]);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ void print_rev(char *s);
 int _strlen(char *s);
 void rev_string(char *s);
 void puts_half(char *str);
+void print_array(int *a, int n);
 
 
 #endif
