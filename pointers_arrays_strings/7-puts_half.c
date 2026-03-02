121e366229058671ddd2c6dd9a82b4f16d61c81e
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-02 20:41:27 +0100

    add: function puts_half print half of char *

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strlen - return len of table
+* @s: table would take mesure
+*
+* Return: int len of table
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+* puts_half - print half table
+* @str: char would print half
+*
+* Return: nothing
+*/
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = _strlen(str) / 2;
+
+	while (str[i] != '\0')
+	{
+		if (i >= j)
+			putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,5 +5,7 @@ int _putchar(char c);
 void print_rev(char *s);
 int _strlen(char *s);
 void rev_string(char *s);
+void puts_half(char *str);
+
 
 #endif
