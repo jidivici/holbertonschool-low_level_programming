71116258e512b1835085a8b258b1dcc45c53155f
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-02 21:13:20 +0100

    add: function print array of int with printf

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,6 +12,10 @@ void print_array(int *a, int n)
 	int i = 0;
 
 	for (i = 0; i < n; i++)
-		printf("%d, ", a[i]);
+		{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+		}
 	printf("\n");
 }
