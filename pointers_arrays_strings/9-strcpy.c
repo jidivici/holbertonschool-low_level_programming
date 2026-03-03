353161ad77eb640b391e0567f02c41b1c00eb828
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-03 14:10:51 +0100

    add: function copy string src to dest

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_array - print int array
-* @a: array you want print
-* @n: taille de l array
+* _strcpy - function copy string src to dest
+* @dest: array you want print
+* @src: taille de l array
 *
 * Return: nothing
 */
-void print_array(int *a, int n)
+char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-
-	for (i = 0; i < n; i++)
-		printf("%d, ", a[i]);
-	printf("\n");
+	char *p = dest;
+	while
+		((*dest++ = *src++));
+	return (p);
 }
