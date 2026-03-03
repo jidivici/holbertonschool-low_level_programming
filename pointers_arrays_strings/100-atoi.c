d2351222d875f76ac6699f0b47e6bbfb0ad0adcb
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-03 16:26:30 +0100

    add: function return first int present in a char *

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* _atoi - function took char return first int content in char
+* @s: array you want to take the first int
+*
+* Return: int
+*/
+int _atoi(char *s)
+{
+	int	i = 0, result = 0, find = 0, sign = 1;
+
+	while (*s)
+		{
+		if (*s == '-' && find == 0)
+			sign = -sign;
+		if (*s >= '0' && *s <= '9')
+		{
+			result = result * 10 + (*s - '0');
+			find = 1;
+		}
+		else if (find == 1)
+			return (sign * result);
+		s++;
+		}
+	return (sign * result);
+}
