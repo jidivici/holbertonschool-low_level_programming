bd9d9c07ca9e69791a96c41e70e78e829da6e836
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-04 18:44:45 +0100

    add: function capitalizes the first letter of each word in a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * separator - checks if a character is a word separator
+ * @c: character to check
+ *
+ * Return: 1 if c is a separator, 0 otherwise
+ */
+int separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i = 0;
+
+	for (; separators[i] != '\0'; i++)
+	{
+		if (separators[i] == c)
+			return (1);
+	}
+	return (0);
+}
+/**
+ * cap_string - capitalizes the first letter of each word in a string
+ * @s: string to modify
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z' && separator(s[i - 1]))
+			s[i] = (s[i] - 32);
+		i++;
+	}
+	return (s);
+}
