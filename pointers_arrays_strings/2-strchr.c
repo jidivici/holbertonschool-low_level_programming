db8197c702b40aa9b2055c6353bdd660265ed282
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-05 15:20:49 +0100

    add: function locate first occur of char in s

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -18,17 +18,3 @@ char *_strchr(char *s, char c)
 	}
 	return (NULL);
 }
-
-int main(void)
-{
-	char *s = "hello";
-	char *f;
-
-	f = _strchr(s, 'l');
-
-	if (f != NULL)
-	{
-		printf("%s\n", f);
-	}
-	return (0);
-}
