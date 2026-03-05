8e0b8ba3f3f841e5c994d806e8989b00a54bad0b
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-05 11:20:02 +0100

    fix: funciton encodes a string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - encodes a string into 1337 (leet) speak
+ * @s: string to encode
+ *
+ * Return: pointer to the modified string
+ */
+char *leet(char *s)
+{
+	int i = 0, j = 0;
+	char *letters = "aAeEoOtTlL";
+	char *numbers = "4455007711";
+
+	for (; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (letters[j] == s[i])
+				s[i] = numbers[j];
+		}
+	}
+	return (s);
+}
