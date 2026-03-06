b7196c9c5ad6c5d80e0fad77bd5e170f7d0ae36f
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-06 13:59:28 +0100

    add: function locates a substring and return a pointer to the beginning of the located substring, or NULL if the substring is not found

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to find
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	if (needle[0] == '\0')
+		return (haystack);
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+			{
+				for (j = 0; needle[j] != '\0'; j++)
+					{
+					if (haystack[i + j] != needle[j] || needle[j] == '\0')
+							break;
+					}
+				if (needle[j] == '\0')
+					return (&haystack[i]);
+			}
+	i++;
+	}
+	return (NULL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,5 +20,6 @@ char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif
