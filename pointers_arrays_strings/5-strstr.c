edcaaa6d0d03415734cd6617788a2661b504a1fa
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-06 14:01:05 +0100

    add: function locates a substring and return a pointer to the beginning of the located substring, or NULL if the substring is not found

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -17,15 +17,15 @@ char *_strstr(char *haystack, char *needle)
 	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
 			{
-				for (j = 0; needle[j] != '\0'; j++)
-					{
-					if (haystack[i + j] != needle[j] || needle[j] == '\0')
-							break;
-					}
-				if (needle[j] == '\0')
-					return (&haystack[i]);
+				if (haystack[i + j] != needle[j] || needle[j] == '\0')
+					break;
 			}
+			if (needle[j] == '\0')
+				return (&haystack[i]);
+		}
 	i++;
 	}
 	return (NULL);
