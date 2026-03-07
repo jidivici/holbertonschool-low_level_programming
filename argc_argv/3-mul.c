bd17c479b160984203f2b8691f7bdd2243200234
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-07 16:02:27 +0100

    add: function  prints All arguments multiplicated together

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include "main.h"
 /**
+* _atoi - function took char return first int content in char
+* @s: array you want to take the first int
+*
+* Return: int
+*/
+unsigned long _atoi(char *s)
+{
+	int find = 0, sign = 1;
+	unsigned long result = 0;
+
+	while (*s)
+	{
+		if (*s == '-' && find == 0)
+			sign = -sign;
+		if (*s >= '0' && *s <= '9')
+		{
+			result = result * 10 + (*s - '0');
+			find = 1;
+		}
+		else if (find == 1)
+			return (sign * result);
+		s++;
+	}
+	return (sign * result);
+}
+
+/**
  * main - prints multiplication of argumments
  * @argc: number of command line arguments
  * @argv: array of command line arguments
