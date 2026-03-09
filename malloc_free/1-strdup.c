d6599cac4781c6397710e2444a94476a74f9b118
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-09 14:35:03 +0100

    add: function create an array and copy the char * stocked in the stack

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the duplicated string, or NULL if str is NULL
+ * or if memory allocation fails
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	array = malloc(sizeof(char) * (i + 1));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		array[i] = str[i];
+	array[i] = '\0';
+	return (array);
+}
