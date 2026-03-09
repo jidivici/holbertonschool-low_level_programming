b326e47a8cbe8cf793e2c793bffa6de26849f5cd
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-09 13:30:32 +0100

    add: function create an array with a caracter

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,50 +1,20 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * create_array - creates an array of chars and initializes it with a char
+ * @size: size of the array
+ * @c: character used to fill the array
  *
- * Return: Nothing.
+ * Return: pointer to the array, or NULL if size is 0 or allocation fails
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
+	unsigned int i = 0;
+	char *array = malloc(size * sizeof(char));
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *buffer;
-
-	buffer = create_array(98, 'H');
-	if  (buffer == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	simple_print_buffer(buffer, 98);
-	free(buffer);
-	return (0);
+	if (array == NULL || size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
 }
