328523dea5978c19a1d3e314d36d2a662e7b8465
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-14 18:46:33 +0100

    fix: free all cellule have been allocated

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -44,7 +44,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = _strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 	new_dog->owner = _strdup(owner);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
 	new_dog->age = age;
 	return (new_dog);
 }
