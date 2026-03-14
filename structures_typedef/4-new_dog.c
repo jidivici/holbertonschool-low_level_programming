0dfb26309ed549f5fa3336ee05e0cf26c42494fe
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-14 18:42:20 +0100

    fix: function strdup on the file new.dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the duplicated string, or NULL if str is NULL
+ * or if memory allocation fails
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	array = malloc(sizeof(char) * (i + 1));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		array[i] = str[i];
+	array[i] = '\0';
+	return (array);
+}
+
+/**
  * new_dog - create a new dog structure
  * @name: name of the dog
  * @age: age of the dog
--- structures_typedef/README.md
+++ structures_typedef/README.md
@@ -1,3 +1,3 @@
-Those task will introduce to 
+Those task will introduce to
 What are structures, when, why and how to use them
 How to use typedef
