dae31388424713371f31825081d5668c374f21a9
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-14 18:30:22 +0100

    add: declaration of init dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,6 +17,7 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
