8aee5f666598285e4a70029ca2be18c77eb5db64
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-15 11:17:51 +0100

    add: function puts_recursive

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,11 +1,12 @@
-include "main.h"
-/**
-* main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+#include "main.h"
+
+void _puts_recursion(char *s)
 {
-	_puts_recursion("Puts with recursion");
-	return (0);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
