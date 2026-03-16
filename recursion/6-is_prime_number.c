472cc33fdfbab14133af3f49e566032f91e65d1f
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-16 13:07:54 +0100

    add: function is_prime_number check and call recursive found_prime-number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,17 +1,29 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * found_prime_number - recursively checks if n is divisible by i
+ * @n: number to test for primality
+ * @i: current divisor being tested
+ *
+ * Return: 1 if n is prime, 0 if n is not prime
+ */
 int found_prime_number(int n, int i)
 {
 	if (i * i > n)
 		return (1);
 	if (n % i == 0)
 		return (0);
-	return found_prime_number(n, ++i);
+	return (found_prime_number(n, ++i));
 }
-
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: number to test for primality
+ *
+ * Return: 1 if n is prime, 0 if n is not prime
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return found_prime_number(n, 2);
+	return (found_prime_number(n, 2));
 }
