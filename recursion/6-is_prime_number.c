55209b2ec9791a5fe34e946050206a1736203953
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-16 13:06:38 +0100

    add: function is_prime_number check and call recursive found_prime-number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,30 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+int found_prime_number(int n, int i)
 {
-	int r;
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return found_prime_number(n, ++i);
+}
 
-	r = is_prime_number(1);
-	printf("%d\n", r);
-	r = is_prime_number(1024);
-	printf("%d\n", r);
-	r = is_prime_number(16);
-	printf("%d\n", r);
-	r = is_prime_number(17);
-	printf("%d\n", r);
-	r = is_prime_number(25);
-	printf("%d\n", r);
-	r = is_prime_number(-1);
-	printf("%d\n", r);
-	r = is_prime_number(113);
-	printf("%d\n", r);
-	r = is_prime_number(7919);
-	printf("%d\n", r);
-	return (0);
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return found_prime_number(n, 2);
 }
--- recursion/main.h
+++ recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int found_sqrt(int n, int i);
+int is_prime_number(int n);
+int found_prime_number(int n, int i);
 
 #endif
