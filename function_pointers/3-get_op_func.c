9ba5a4c2a6eb89daa7921cd6ef590d65e420f734
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-18 18:14:20 +0100

    add: function get_op_function init struct ops return the function associated to the char

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+/**
+ * get_op_func - selects the correct operation function
+ * @s: operator string
+ *
+ * Return: pointer to the corresponding function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+	i++;
+	}
+	return (NULL);
+}
