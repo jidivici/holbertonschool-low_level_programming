61c79df85e435268e498a1b1d1efbb4a15e06e69
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-18 19:39:01 +0100

    fix:  case when one str is null return nil with ternaire operation

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,8 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(args, char *);
-		if (s != NULL)
-			printf("%s ", s);
+		s ? printf("%s", s) : printf("(nil)");
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
