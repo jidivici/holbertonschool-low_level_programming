ad9fb38acabaf784951ddfed9c8b0aad6c105bea
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-25 18:32:23 +0100

    add: function list_len of single linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
