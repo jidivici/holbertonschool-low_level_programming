6f337e3e2d7073d9030c2507bff22d7089739eef
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-25 17:31:48 +0100

    add: first push init files

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _strlen(char *str);
+char *_strdup(char *str);
+/**
+ * struct dog - structure representing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
