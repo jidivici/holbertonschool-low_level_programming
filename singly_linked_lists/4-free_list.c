d1e674c33f7184f1b2de4d15a8695aa56f4d21fb
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-03-27 16:29:51 +0100

    add: function free_list parcours and free with current

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+/**
+ * free_list - Frees all nodes of a linked list
+ * @head: pointer to the head of the list
+ *
+ * This function traverses the linked list and frees each node,
+ * including the memory allocated for the stored string (if any).
+ * After execution, all memory used by the list is released.
+ */
 void free_list(list_t *head)
 {
 	list_t *curr;
