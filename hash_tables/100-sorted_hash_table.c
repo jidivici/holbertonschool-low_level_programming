e03d0531390bcbca6d94a89bdf8e1f0ded53a4f4
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-05 11:54:12 +0200

    fix: requirement

--- hash_tables/100-sorted_hash_table.c
+++ hash_tables/100-sorted_hash_table.c
@@ -3,24 +3,6 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
- * key_index - returns the index of a key
- * @key: key to hash
- * @size: size of array
- *
- * Return: index
- */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
-	unsigned long int hash;
-
-	if (key == NULL || *key == '\0' || size == 0)
-		return (0);
-
-	hash = hash_djb2(key);
-	return (hash % size);
-}
-
-/**
  * shash_table_create - Crée une table de hachage
  * @size: Taille du tableau (nombre de buckets)
  *
@@ -46,24 +28,6 @@ shash_table_t *shash_table_create(unsigned long int size)
 	for (i = 0; i < hash_table->size; i++)
 		hash_table->array[i] = NULL;
 	return (hash_table);
-}
-/**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
- */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
 }
 /**
  * create_node - creates a new hash node
