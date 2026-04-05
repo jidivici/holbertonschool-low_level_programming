b727abc4c9b6154fce1db05291d7f1b13d418d36
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-05 09:57:49 +0200

    add: function free hash_table_delete free all node from ht

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -2,3 +2,32 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
+/**
+ * hash_table_delete - free all node from hash table
+ * @ht: hash table
+ *
+ * Return: nothing
+ */
+void hash_table_delete(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *curr;
+	hash_node_t *tmp;
+
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		tmp = ht->array[i];
+		while (curr)
+		{
+			tmp = curr->next;
+			free(curr->key);
+			free(curr->value);
+			free(curr);
+			curr = tmp;
+		}
+	}
+	free(ht->array);
+}
