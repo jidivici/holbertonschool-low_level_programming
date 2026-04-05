45e06dc9a56e5ce6e089e6834893af718b2ffb9e
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-05 14:32:44 +0200

    add: function shash in file hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -4,7 +4,6 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -81,4 +80,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void shash_table_print(const shash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+
 #endif
