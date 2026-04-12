829ad2d582888981012132cb93448b9db91e9776
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-12 13:20:44 +0200

    fix: return -1 in case of fail

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, len = 0;
 
 	if (!filename)
-		return (0);
+		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 		if (write(fd, text_content, len) == -1)
 		{
 			close(fd);
-			return (0);
+			return (-1);
 		}
 	}
 	close(fd);
