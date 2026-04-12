6fe7bf71e9ae6513bfd4383982a7d872796d4669
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-12 12:37:59 +0200

    fix: write use fonction

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,25 +17,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		ssize_t n;
 		char *buff = NULL;
 
-		if (!filename)
-			return (0);
 		fd = open(filename, O_RDONLY);
 		if (fd == -1)
 			return (0);
+		if (!filename)
+			return (0);
+		n = read(fd, buff, letters);
 		buff = malloc(letters * sizeof(char));
 		if (!buff)
 		{
 			close(fd);
 			return (0);
 		}
-		n = read(fd, buff, letters);
 		if (n == -1)
 		{
 			close(fd);
 			free(buff);
 			return (0);
 		}
-		write(1, buff, n);
+		write(STDOUT_FILENO, buff, n);
 		free(buff);
 		close(fd);
 		return (n);
