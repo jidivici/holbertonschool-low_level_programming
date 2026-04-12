9288b9fdfce02aee43a767efc763554029dd1e8d
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-12 12:39:23 +0200

    fix: order of if clue

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,22 +17,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		ssize_t n;
 		char *buff = NULL;
 
+		if (!filename)
+			return (0);
 		fd = open(filename, O_RDONLY);
 		if (fd == -1)
 			return (0);
-		if (!filename)
-			return (0);
 		n = read(fd, buff, letters);
-		buff = malloc(letters * sizeof(char));
-		if (!buff)
-		{
-			close(fd);
-			return (0);
-		}
 		if (n == -1)
 		{
 			close(fd);
 			free(buff);
+			return (0);
+		}
+		buff = malloc(letters * sizeof(char));
+		if (!buff)
+		{
+			close(fd);
 			return (0);
 		}
 		write(STDOUT_FILENO, buff, n);
