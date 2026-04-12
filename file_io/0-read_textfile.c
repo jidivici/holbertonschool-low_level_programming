1b60eeb35c81fe230fd13874172b690b6db08b68
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-12 12:43:17 +0200

    fix: sortie use by write order of if

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -22,13 +22,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		fd = open(filename, O_RDONLY);
 		if (fd == -1)
 			return (0);
-		n = read(fd, buff, letters);
 		buff = malloc(letters * sizeof(char));
 		if (!buff)
-		{
-			close(fd);
 			return (0);
-		}
+		n = read(fd, buff, letters);
 		if (n == -1)
 		{
 			close(fd);
