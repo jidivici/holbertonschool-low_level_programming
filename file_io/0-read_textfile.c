d4eb5aec81c75f7ca26d339f961b749fa797c7af
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-12 12:27:16 +0200

    fix: return of

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		char *buff = NULL;
 
 		if (!filename)
-			return (-1);
+			return (0);
 		fd = open(filename, O_RDONLY);
 		if (fd == -1)
 			return (-1);
